c145fd6de17594900c9f144159e34218170ea30d
DESKTOP-1MR5D0H\tuananhnk-01 <anhnkthe151369@fpt.edu.vn>
2023-11-08 09:09:30 +0700

    InitialProject

---
+++ CrystalReportConnectionFee/CrystalReportConnectionFee/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrystalReportConnectionFee.Startup))]
+namespace CrystalReportConnectionFee
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ CrystalReportConnectionFee/CrystalReportConnectionFee/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+public partial class _Default : Page
+{
+    string connectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.144.13.7)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SID=ee)));User Id=VM;Password=Oracle12321";
+    ReportDocument crystalReport = new ReportDocument();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void VTL_A_1_Click(object sender, EventArgs e)
+    {
+        string sqlQuery = "SELECT * FROM ";
+
+        using (OracleConnection connection = new OracleConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                using (OracleCommand command = new OracleCommand(sqlQuery, connection))
+                {
+                     
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+
+                        DataSet ds = new DataSet();
+
+                    }
+                }
+                connection.Close();
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
